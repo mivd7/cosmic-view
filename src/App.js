3105fb65f4abcfc4f7492f4bfd9632d29f65b5eb
MIVD <mi.vandeurzen@gmail.com>
2020-09-27 15:25:26 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, {
+  useEffect,
+  useRef
+} from 'react';
+import * as d3 from 'd3';
+import { Celestial } from 'd3-celestial/celestial.min.js';
+import config from './config';
 import logo from './logo.svg';
+import * as json from './planets.json';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const d3Ref = useRef()
+  config.width = config.width !== 0 ? config.width : window.screen.width - 15 ;
+  const celestial = new Celestial();
+  console.log(celestial);
+  console.log(json);
+  useEffect(() => {
+    const date = '2020-09-27 13:55:43 +0200'
+    // console.log(earth);
+    const dateEl = document.getElementById('#date');
+    console.log(dateEl)
+    try {
+      d3.select(d3Ref.current)
+        .append(celestial.display(config))
+    } catch(err) {
+      console.error(err);
+    }
+  })
+
+  return ( 
+    <div className='celestial-container' ref={d3Ref} >
     </div>
   );
 }
