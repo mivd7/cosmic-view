c2d077a8a4f1d8c570bf77d953930b809d9f7c3b
MIVD <mi.vandeurzen@gmail.com>
2020-09-27 15:28:44 +0200

    load simple map

--- src/App.js
+++ src/App.js
@@ -5,22 +5,13 @@ import React, {
 import * as d3 from 'd3';
 import { Celestial } from 'd3-celestial/celestial.min.js';
 import config from './config';
-import logo from './logo.svg';
-import * as json from './planets.json';
-
 import './App.css';
 
 function App() {
   const d3Ref = useRef()
   config.width = config.width !== 0 ? config.width : window.screen.width - 15 ;
   const celestial = new Celestial();
-  console.log(celestial);
-  console.log(json);
   useEffect(() => {
-    const date = '2020-09-27 13:55:43 +0200'
-    // console.log(earth);
-    const dateEl = document.getElementById('#date');
-    console.log(dateEl)
     try {
       d3.select(d3Ref.current)
         .append(celestial.display(config))
